895eed9ba502e3902799a899097a8883bc8178c5
RidwanC134 <ridwanhh10@gmail.com>
2023-03-16 16:05:35 +0700

    create procedure untuk input

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -25,6 +25,17 @@ void input() {          //procedure untuk input
         cin >> a[i];                                        //Input dari pengguna
     }
 }
+void bubbleSortArray() {                                //procedure untuk mengurutkan array dengan metode bubble sort
+    for (int i = 1; i < n; i++) {                       //Looping dengan i dimulai dari 1 hingga n-1
+        for (int j = 0; j < n - i; j++) {               //Looping dengan j dimulai dari 0 hingga n-i-1
+            if (a[j] > a[j + 1]) {                      //Jika nilai a[j] lebih besar dari a[j+1]
+                int temp = a[j];                        //Simpan nilai a[j] ke variable sementara temp
+                a[j] = a[j + 1];                        //Assign nilai a[j+1] ke a[j]
+                a[j + 1] = temp;                        //Assign nilai temp ke a[j+1]
+            }
+        }
+    }
+}
 
 
 
